bd3489891287baadbe85798940c17f2fc6978089
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-09-18 21:58:09 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+password= "Ght7$Tyb1@Qwz"
+password="kL!@56Y&Hpo*9v7"
+password=abcd@1234
+password='Sunshine@2022'
+password='abcd1234'
+password='123456'
+password=user_input_1234
+password='password@2345'
